61a36e93c8583958d8221273cce000b65cd97e9a
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-27 17:41:25 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { cors } from '../middleswares/cors'
+import { contentType } from '../middleswares/content-type'
 
 export default (app: Express): void => {
   app.use(cors)
+  app.use(contentType)
 }
