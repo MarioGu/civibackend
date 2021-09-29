4d8280521392a9a8f17c156cb2ed044af5f7f5a8
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-29 18:43:11 -0300

    feat: add limit to messages

--- src/data/usecases/local-messages.ts
+++ src/data/usecases/local-messages.ts
@@ -3,18 +3,24 @@ import { Messages } from '../../domain/usecases/messages'
 
 export class LocalMessages implements Messages {
   async post (start: number = 0): Promise<MessageModel[]> {
-    let messages: MessageModel[] = []
 
-    for (let i = 1; i < 21; i++) {      
-      let message: MessageModel = {
-        id: (20 * start) + i,
-        timestamp: new Date(2020, 5, i + 10, start * i + 5, 0, 0).getTime(),
-        subject: `Assunto ${((20 * start) + i)}`,
-        detail: `Detalhe ${((20 * start) + i)}`
+    if (start < 5) {
+
+      let messages: MessageModel[] = []
+  
+      for (let i = 1; i < 21; i++) {      
+        let message: MessageModel = {
+          id: (20 * start) + i,
+          timestamp: new Date(2020, 5, i + 10, start * i + 5, 0, 0).getTime(),
+          subject: `Assunto ${((20 * start) + i)}`,
+          detail: `Detalhe ${((20 * start) + i)}`
+        }
+        messages.push(message)
       }
-      messages.push(message)
+      
+      return messages
+    } else {
+      return []
     }
-    
-    return messages
   }
 }
